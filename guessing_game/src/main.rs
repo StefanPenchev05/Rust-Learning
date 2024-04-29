6c619f197cde797d82a58158616395bbee4debfe
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-04-29 11:38:32 +0300

    Add rand dependency and implement number guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,15 @@
 // we use "use" keyword to import dependecies like io 
 // the std is standard lib, for now we will ude std::io and esspecially stdin
-use std::io::stdin;
+use std::{cmp::Ordering, io::stdin};
+
+// Using the dependecy for random value
+use rand::Rng;
 
 fn main() {
     println!("Hello to my guessing game!");
     println!("Please enter your number: ");
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
 
     // let - is creating new variable, but in rust the variables are imutable by default
     // so to make them mutable we need to use the keyword mut
@@ -19,6 +24,14 @@ fn main() {
     stdin()
         .read_line(&mut number)
         .expect("Faild to read user input");
+
+    let guess: u32 = number.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Your number is small"),
+        Ordering::Greater => println!("Your number is a bigger"),
+        Ordering::Equal => println!("You win!!!")
+    }
 
     println!("Your guessed number is {number}");
 }
