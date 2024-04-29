da786fe1ba852afb7d7bc7351408948c30767694
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-04-29 11:44:20 +0300

    Refactor input handling and add loop in number guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,4 @@
-// we use "use" keyword to import dependecies like io 
+// we use "use" keyword to import dependecies like io
 // the std is standard lib, for now we will ude std::io and esspecially stdin
 use std::{cmp::Ordering, io::stdin};
 
@@ -7,31 +7,39 @@ use rand::Rng;
 
 fn main() {
     println!("Hello to my guessing game!");
-    println!("Please enter your number: ");
-
-    let secret_number = rand::thread_rng().gen_range(1..=10);
 
     // let - is creating new variable, but in rust the variables are imutable by default
     // so to make them mutable we need to use the keyword mut
     // The String::new() create new isntace of String, and ny using the mut we make the number variable
     // to recive data from type string and it is mutable
     let mut number: String = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=10);
 
-    // using stdin for getting the input data from user
-    // Read Line is getting the data and puts it in the variabel, and we need to pass the memory address
-    // of the variable, by saying &mut we are giving the mutable memory adress of the var
-    // Except is for handling the errors
-    stdin()
-        .read_line(&mut number)
-        .expect("Faild to read user input");
+    loop {
+        println!("Please enter your number: ");
 
-    let guess: u32 = number.trim().parse().expect("Please type a number!");
+        // using stdin for getting the input data from user
+        // Read Line is getting the data and puts it in the variabel, and we need to pass the memory address
+        // of the variable, by saying &mut we are giving the mutable memory adress of the var
+        // Except is for handling the errors
+        stdin()
+            .read_line(&mut number)
+            .expect("Faild to read user input");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Your number is small"),
-        Ordering::Greater => println!("Your number is a bigger"),
-        Ordering::Equal => println!("You win!!!")
+        let guess: u32 = match number.trim().parse(){
+            Ok(number) => number,
+            Err(_) => continue
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your number is small"),
+            Ordering::Greater => println!("Your number is a bigger"),
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            }
+        }
     }
 
-    println!("Your guessed number is {number}");
+    println!("You guessed: {number}");
 }
