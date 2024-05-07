e4bf6d89e10d0de0c0e8df6f1b9a29645faf92cf
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-05-08 02:18:32 +0300

    Refactor main.rs to improve ownership understanding and add functions

--- OwnerShip/Rules/src/main.rs
+++ OwnerShip/Rules/src/main.rs
@@ -1,16 +1,66 @@
 fn main() {
+    // Create a mutable string
     let mut name = String::from("Stefan Penchev");
-    name.push_str(", hello my frined"); // append other string to already made up string
+    // Append another string to the existing string
+    name.push_str(", hello my friend");
 
+    // Create a variable
     let num = 3;
-    
-    // The rust compiler will destroy the name before goes to heap
-    // so is some veriable get the value of name, it gets the address to the memroy and len and capacity
 
-    // shallow copy - copying the pointer, length and capacity
-    // deep copy- copying the data and takes memory as the first var
+    // Shallow copying: copying the pointer, length, and capacity
+    // The Rust compiler will destroy the original variable, so if another variable gets the value of name, it gets the address to the memory, length, and capacity
+    let second_name = name;
 
-    let second_name = name; // shallow copying
-    let second_num = num.clone(); // deep copying
-    println!("{second_name} {num} {second_num}");
+    // Deep copying: copying the data and takes the same amount of memory as the original variable
+    let second_num = num;
+
+    // Print the variables
+    println!("{}, {}", second_name, second_num);
+
+    // Some types, like i32 and boolean, have the Copy trait and are directly deep copied
+    let s = String::from("Lets try this ownership");
+    // The function takes_ownership takes the ownership of s and s cannot be used anymore in this scope
+    takes_ownership(s);
+
+    let n: i32 = 4324;
+    // The function makes_copy makes a copy of n
+    makes_copy(n);
+
+    // Print the variable n
+    println!("{}", n);
+
+    // Create a new string
+    let s2 = gives_ownership();
+    // Print the string
+    println!("{}", s2);
+
+    // Call a function that takes a string and returns it
+    let s3 = takes_and_gives_ownership(String::from("Takes and Gives back ownership"));
+    // Print the returned string
+    println!("{}", s3);
+}
+
+// Function that takes ownership of a string
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+// Function that makes a copy of an i32
+fn makes_copy(n: i32) {
+    println!("{}", n)
+}
+
+// Function that creates a string and gives ownership to the caller
+fn gives_ownership() -> String {
+    // Create a new string
+    let s = String::from("Giving ownership to var");
+
+    // Return the string, giving ownership to the caller
+    s
+}
+
+// Function that takes a string and gives ownership back to the caller
+fn takes_and_gives_ownership(s: String) -> String {
+    // Return the string, giving ownership back to the caller
+    s
 }
