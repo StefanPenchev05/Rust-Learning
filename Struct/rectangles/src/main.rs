f356ec126119253d874287eafac270d2118b6c3b
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-06-06 12:25:22 +0300

    Make area finder of Rectangle with Struct and Enum

---
+++ Struct/rectangles/src/main.rs
@@ -0,0 +1,49 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// Define an enum that can be either a Rectangle or two u32 values
+enum RectangleDefinition {
+    Dimensions(u32, u32),
+    Rectangle(Rectangle),
+}
+
+fn main() {
+    // This is the most simpole way of doing this
+    let width = 32;
+    let height = 14;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_of_rectangle(RectangleDefinition::Dimensions(width, height))
+    );
+
+    // Implement finding the are of rectangle with struct
+    let rec1 = build_rectangle_struct(42, 18);
+    println!(
+        "Strcut Rectangle with values width: {} and height: {} is\n {:#?}",
+        rec1.width, rec1.height, rec1
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_of_rectangle(RectangleDefinition::Rectangle(rec1))
+    );
+}
+
+// This function calculates the area of a rectangle.
+// It accepts a RectangleDefinition enum which can be either a Rectangle struct or two u32 values (width and height).
+fn area_of_rectangle(def: RectangleDefinition) -> u32 {
+    // Use pattern matching to determine the type of RectangleDefinition
+    match def {
+        // If it's Dimensions, multiply the width and height to get the area
+        RectangleDefinition::Dimensions(width, height) => width * height,
+        // If it's a Rectangle, multiply the width and height fields of the Rectangle to get the area
+        RectangleDefinition::Rectangle(rect) => rect.width * rect.height,
+    }
+}
+
+fn build_rectangle_struct(width: u32, height: u32) -> Rectangle {
+    Rectangle { width, height }
+}
