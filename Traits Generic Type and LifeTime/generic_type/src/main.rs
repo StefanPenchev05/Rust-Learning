52390f94702cc80df356d72cb9a2e2317df5e93a
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-06-14 06:48:31 +0300

    Added Generic Type

---
+++ Traits Generic Type and LifeTime/generic_type/src/main.rs
@@ -0,0 +1,59 @@
+use std::fmt::Debug;
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+
+struct Point<T, U>{
+    x_position: T,
+    y_position: U
+}
+
+impl<T: Debug, U: Debug> Point<T, U> {
+    fn new(x_position: T, y_position: U) -> Self{
+        Self{
+            x_position,
+            y_position
+        }
+    }
+    fn print_point(&self){
+        println!("x: {:?}, y: {:?}", self.get_x(), self.get_y())
+    }
+
+    fn get_x(&self) -> &T{
+        &self.x_position
+    }
+
+    fn get_y(&self) -> &U{
+        &self.y_position
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_each_other(&self) -> f32{
+        (self.x_position.powi(2) + self.y_position.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let vec = vec![1,2,3,4,14,75];
+    println!("{}", largest(&vec));
+
+    let vec = vec!['c', 'g', '/', 'f'];
+    println!("{}", largest(&vec));
+
+    let point = Point::new(4, 7.3);
+    point.print_point();
+
+    let point = Point::new(4.5 as f32, 6.5 as f32);
+    println!("The distance from x: {} and y: {} is {}",point.get_x(), point.get_y(), point.distance_from_each_other());
+}
